8ab99c9e01b9c3ecf0844e880cd8034370ef40b5
Justin Pratley <justinpratley@yahoo.com>
2017-11-23 01:22:34 -0500

    Operations for Cars and Users

--- javafiles/Employee.java
+++ javafiles/Employee.java
@@ -1,39 +1,98 @@
-import java.util.*;
+import java.sql.*;
 public class Employee {
-	private static int employeeCount = 0;
-	private int idNumber;
-	private String designation;
-	private String password;
-	private String fName;
-	private String lName;
-	private double hours;
-	Scanner scan = new Scanner(System.in);
-	 
-	public Employee(){
-		String temp1, temp2;
-		int confirm = 1;
-		this.designation = "Employee";
-		System.out.print("First Name: ");
-		this.fName = scan.nextLine();
-		System.out.print("Last Name: ");
-		this.lName = scan.nextLine();
-		System.out.print("Create Password: ");
-		temp1 = scan.nextLine();
-		System.out.print("Confirm Password: ");
-		temp2 = scan.nextLine();
-		if(temp1.equals(temp2))
-			confirm = 0;
-		while(confirm == 1){
-			System.out.println("Passwords do not match. Try Again.");
-			System.out.print("Create Password: ");
-			temp1 = scan.nextLine();
-			System.out.print("Confirm Password: ");
-			temp2 = scan.nextLine();
-			if(temp1.equals(temp2))
-				confirm = 0;
-		}
-		employeeCount++;
-		this.idNumber = employeeCount;
-	}
 
+	
+	public static void setUsername(String desired, String current){
+		String sql = "UPDATE User SET USERNAME = ?" + "WHERE USERNAME = ?";
+		try (Connection conn = DriverManager.getConnection(DBTools.url);
+				PreparedStatement pstmt = conn.prepareStatement(sql)){
+			pstmt.setString(1, desired);
+			pstmt.setString(2, current);
+			pstmt.executeUpdate();
+		} catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+				
+	}
+	
+	
+	public static void setPassword(String password, String username){
+		String sql = "UPDATE User SET PASSWORD = ?" + "WHERE USERNAME = ?";
+		try (Connection conn = DriverManager.getConnection(DBTools.url);
+				PreparedStatement pstmt = conn.prepareStatement(sql)){
+			pstmt.setString(1, password);
+			pstmt.setString(2, username);
+			pstmt.executeUpdate();
+		} catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+				
+	}
+	
+	public static void setFirst(String name, String username){
+		String sql = "UPDATE User SET FNAME = ?" + "WHERE USERNAME = ?";
+		try (Connection conn = DriverManager.getConnection(DBTools.url);
+				PreparedStatement pstmt = conn.prepareStatement(sql)){
+			pstmt.setString(1, name);
+			pstmt.setString(2, username);
+			pstmt.executeUpdate();
+		} catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+				
+	}
+	
+	public static void setLast(String name, String username){
+		String sql = "UPDATE User SET LNAME = ?" + "WHERE USERNAME = ?";
+		try (Connection conn = DriverManager.getConnection(DBTools.url);
+				PreparedStatement pstmt = conn.prepareStatement(sql)){
+			pstmt.setString(1, name);
+			pstmt.setString(2, username);
+			pstmt.executeUpdate();
+		} catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+				
+	}
+	
+	public static void setDesignation(String des, String username){
+		String sql = "UPDATE User SET DESIGNATION = ?" + "WHERE USERNAME = ?";
+		try (Connection conn = DriverManager.getConnection(DBTools.url);
+				PreparedStatement pstmt = conn.prepareStatement(sql)){
+			pstmt.setString(1, des);
+			pstmt.setString(2, username);
+			pstmt.executeUpdate();
+		} catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+				
+	}
+	
+	public static void setRate(double rate, String username){
+		String sql = "UPDATE User SET RATE = ?" + "WHERE USERNAME = ?";
+		try (Connection conn = DriverManager.getConnection(DBTools.url);
+				PreparedStatement pstmt = conn.prepareStatement(sql)){
+			pstmt.setDouble(1, rate);
+			pstmt.setString(2, username);
+			pstmt.executeUpdate();
+		} catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+				
+	}
+	
+	public static void setHours(double hours, String username){
+		String sql = "UPDATE User SET HOURS = ?" + "WHERE USERNAME = ?";
+		try (Connection conn = DriverManager.getConnection(DBTools.url);
+				PreparedStatement pstmt = conn.prepareStatement(sql)){
+			pstmt.setDouble(1, hours);
+			pstmt.setString(2, username);
+			pstmt.executeUpdate();
+		} catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+				
+	}
+	
+	
 }
