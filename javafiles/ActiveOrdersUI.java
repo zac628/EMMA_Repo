446f66b5e3e51c68fa4ed2420ff5fcd8128a4e5c
Justin Pratley <justinpratley@yahoo.com>
2017-11-26 20:57:46 -0500

    Changes for new work orders

---
+++ javafiles/ActiveOrdersUI.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.JComboBox;
+import javax.swing.JSpinner;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+
+public class ActiveOrdersUI extends JFrame {
+
+	private JPanel contentPane;
+
+	public static void go() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ActiveOrdersUI frame = new ActiveOrdersUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ActiveOrdersUI() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(1, 1, 800, 565);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+}	
